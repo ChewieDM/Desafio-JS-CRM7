153f076d756b7e3aa88edbf88f485a9ef140a182
ChewieDM <gabrielkm99@hotmail.com>
2023-07-25 16:52:58 -0300

    add new user to database

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
+// Add new user
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
 // Start server
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`);
