575c6b814ed9b84d16fd1a2f41a21c17a97d4b70
ChewieDM <gabrielkm99@hotmail.com>
2023-07-25 16:54:07 -0300

    fetch and remove by id

--- app.js
+++ app.js
@@ -38,6 +38,25 @@ app.put('/users/:id', (req, res) => {
     res.json(users[index]);
 });
 
+// fetch user by ID
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not Found' });
+    }
+
+    res.json(user);
+});
+
+// remove user by ID
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    users = users.filter(user => user.id !== id);
+    res.json({ message: 'User removed Successfully' });
+});
+
 // Start server
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`);
